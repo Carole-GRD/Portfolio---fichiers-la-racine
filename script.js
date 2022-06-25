12719bd93ca2876d9d8e6857e8aa65ccb8fd3c7b
Carole Gérard <gerard.carole@skynet.be>
2022-06-25 17:14:45 +0200

    Ajout d'un 'scrollspy' dans le fichier script.js

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-// APPRENTISSAGE DU "SMOOTH SCROLL"
-// NE FONCTIONNE PAS !!!
+// =====================================================================================
+// ------------------  "SCROLL SMOOTH"  --------------------------
+//       NE FONCTIONNE PAS !!!  -> voir en ligne 47 index "-1" !! 
+// =====================================================================================
 
 
 // à quoi servent les 3 points devant 'document' ?
 // attribut de propagation qui permettent de développer une expression
 // raccourci syntaxique qui donne le privilège d'obtenir une liste à partir d'un tableau
-const navLinks = [...document.querySelectorAll('nav a')]
-const sections = [...document.querySelectorAll('section')]
-
-// console.log(navLinks[4]);
+const navLinks = [...document.querySelectorAll('nav a')];
+const sections = [...document.querySelectorAll('section')];
+// console.log(navLinks);
 // console.log(sections);
 
 
@@ -18,6 +19,7 @@ let sectionsPosition;
 // ON PLACE CES POSITIONS DANS LA VARIABLE "sectionsPosition" QUE L'ON A DECLAREE CI-DESSUS
 function positionCalculation(){
     sectionsPosition = sections.map(section => section.offsetTop);
+    // console.log(sections);
     // console.log(sectionsPosition);
 }
 positionCalculation();
@@ -26,8 +28,6 @@ On appelle les éléments du tableau "sections" comme on veut...
 Dans ce cas, on les appelle "section", cela a du sens 
 car on demande la position supérieure (offsetTop) de chaque section 
 du fichier HTML. */
-
-
 
 
 navLinks.forEach(link => link.addEventListener('click', addScrollSmooth));
@@ -39,15 +39,59 @@ car on demande d'effectuer une fonction (qu'on appelle addScollSmooth) à chaque
 
 
 function addScrollSmooth(e){
+    
     const linkIndex = navLinks.indexOf(e.target);
-    console.log(linkIndex);      /* pourquoi index -1 pour le premier ?? */
+    /*console.log(linkIndex);*/      /* pourquoi index -1 pour le premier ?? */
+    
     window.scrollTo({
         top: sectionsPosition[linkIndex],
-        behavior: "smooth"
-    })
+        behavior: "smooth",
+    })    
+    
 }
 
 window.addEventListener('resize', positionCalculation);
+
+
+
+
+// =====================================================================================
+// ------------------  "SCROLLSPY"  --------------------------
+//          index "-1" contourné en ligne 88
+// =====================================================================================
+
+let styleScrollSpy = document.querySelector('styleScrollSpy');
+
+const navLinksLi = [...document.querySelectorAll('nav li')];
+// console.log(navLinksLi)
+
+// L'ONGLET "HOME" DOIT AVOIR LE STYLE AU CHARGEMENT DE LA PAGE
+window.addEventListener('load', loadStyleHome);
+function loadStyleHome(){
+    navLinks[1].classList.add('styleScrollSpy');
+    navLinksLi[1].classList.add('styleScrollSpy');
+}
+
+
+// ATTENTION : l'Array "navLinks" est créé dans le code SCROLL SMOOTH (ligne 8)
+navLinks.forEach(link => link.addEventListener('click', addScrollSpy));
+function addScrollSpy(e){
+    for (let i = 0; i < navLinks.length; i++){
+        // retirer le style de tous les liens
+        navLinks[i].classList.remove('styleScrollSpy');
+        navLinksLi[i].classList.remove('styleScrollSpy');
+        // obtenir l'index du lien cliqué
+        const linkIndex = navLinks.indexOf(e.target);
+        // ajouter le style au lien cliqué
+        if (linkIndex >= 1){                                         /* pour éviter l'index "-1" */
+            navLinks[linkIndex].classList.add('styleScrollSpy');
+            navLinksLi[linkIndex].classList.add('styleScrollSpy');
+        }
+    }
+}
+
+
+
 
 
 
